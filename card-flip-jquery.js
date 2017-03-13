371a51da6d67459786da66f2804b9761f5676d8a
Shirley95 <shirleyyang95.sy@gmail.com>
2017-03-13 20:11:25 +0000

    added favicon and 1 card flip

--- card-flip-jquery.js
+++ card-flip-jquery.js
@@ -1,5 +1,14 @@
-$("#card").flip({
+if (typeof jQuery != ‘undefined’) {
+        alert(“jQuery library is loaded!”);
+      }
+      else{
+        alert(“jQuery library is not found!”);
+      }
+
+/* $("#card").flip({
 	axis: 'y',
 	trigger: 'hover',
 	reverse: false
-})
+}) */
+
+
